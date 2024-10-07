8dd1efb8407b2903ea5619d9309e1351d7c1a6fc
hamdan0006 <hamdanahmad0006@gmail.com>
2024-10-07 20:49:09 +0500

    edited

--- Backend/app.js
+++ Backend/app.js
@@ -1,49 +1,20 @@
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const blogRoutes =require("./Routes/route");
-// const cors = require('cors');
-// const multer =require("multer") 
-
-
-// // Load environment variables
-
-// const app = express();
-// const PORT = 3000;
-// app.use(cors());
-
-// // Middleware
-// app.use(express.json()); // Parse JSON bodies
-
-// // MongoDB connection
-// mongoose.connect("mongodb+srv://hamdanahmad0006:hamdan123@cluster0.9zyv6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-//   .then(() => console.log('DB connected successfully'))
-//   .catch(err => console.error('MongoDB connection error:', err));
-
-
-//   app.use('/blogs', blogRoutes);
-
-
-// // Start server
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 const blogRoutes = require('./Routes/route'); // Import the blog routes
 const cors = require('cors');
 const path = require("path");
 const contactRoutes = require('./Routes/contact-route'); 
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
-app.use(express.json()); // Parse JSON bodies
+app.use(express.json()); 
 
 // MongoDB connection
 mongoose.connect("mongodb+srv://hamdanahmad0006:hamdan123@cluster0.9zyv6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
